0a7ca2b2a8a1afc13d924847d571249ef08b3895
Mark Andrews <mjandrews.org@gmail.com>
2021-09-14 17:29:32 +0100

    Update script

--- scripts/bas_script_day_1.R
+++ scripts/bas_script_day_1.R
@@ -207,3 +207,170 @@ log(x)
 x %>% log()
 
 x %>% log() %>% sqrt() %>% sum() %>% log()
+
+blp_df2 <- mutate(blp_df, accuracy = lex == resp)
+blp_df3 <- filter(blp_df2, accuracy == TRUE)
+select(blp_df3, participant, resp, rt)
+
+
+mutate(blp_df, accuracy = lex == resp) %>% 
+  filter(accuracy == TRUE) %>% 
+  select(participant, resp, rt)
+
+blp_df %>% 
+  mutate(accuracy = lex == resp) %>% 
+  filter(accuracy == TRUE) %>% 
+  select(participant, resp, rt)
+
+blp_clean_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/bas2021datascience/master/data/blp-trials-short.txt") %>% 
+  mutate(accuracy = lex == resp) %>% 
+  filter(accuracy == TRUE) %>% 
+  select(participant, resp, rt)
+
+
+# Summarize with summarize ------------------------------------------------
+
+
+summarise(blp_df, 
+          mean_rt = mean(rt, na.rm = T),
+          median_rt = median(rt, na.rm = T),
+          std_rt = sd(rt, na.rm = T),
+          n_resp_w = sum(resp == 'W')
+)
+
+summarise(blp_df,
+          mean_rt = mean(rt, na.rm = T),
+          mean_prev_rt = mean(prev.rt, na.rm = T),
+          mean_rt_raw = mean(rt.raw, na.rm = T)
+)
+
+summarise(blp_df,
+          across(rt:rt.raw, ~ mean(., na.rm = T)
+    )
+)
+
+summarise(blp_df,
+          across(where(is.numeric), ~ mean(., na.rm = T)
+          )
+)
+
+
+summarize(blp_df,
+          across(rt:rt.raw,
+                 list(avg = ~ mean(., na.rm = T),
+                      stdev = ~ sd(., na.rm = T))
+          )
+)
+
+
+# split-apply-combine with group_by and summarize -------------------------
+
+group_by(blp_df, lex) %>% 
+  summarize(avg = mean(rt, na.rm = T),
+            stdev = sd(rt, na.rm = T),
+            spell = spell[1])
+
+group_by(blp_df, lex) %>% 
+  summarize(across(rt:rt.raw,
+                   list(avg = ~ mean(., na.rm = T),
+                        stdev = ~ sd(., na.rm = T))
+  )
+)
+
+
+# Combining data frames ---------------------------------------------------
+
+Df_1 <- tibble(x = c(1, 2, 3),
+               y = c(2, 7, 1),
+               z = c(0, 2, 7))
+
+Df_2 <- tibble(y = c(5, 7),
+               z = c(6, 7),
+               x = c(1, 2))
+
+Df_3 <- tibble(a = c(5, 6, 1),
+               b = c('a', 'b', 'c'),
+               c = c(T, T, F))
+
+Df_a <- tibble(x = c(1, 2, 3),
+               y = c('a', 'b', 'c'))
+
+Df_b <- tibble(x = c(2, 3, 4),
+               z = c('d', 'e', 'f'))
+
+Df_4 <- tibble(x = c(1, 2, 3),
+               y = c(2, 7, 1),
+               z = c(0, 2, 7))
+
+Df_5 <- tibble(a = c(1, 1, 7),
+               b = c(2, 3, 7),
+               c = c('a', 'b', 'c'))
+
+Df_6 <- tibble(x = c(1, 2, 3),
+               y = c(4, 5, 6),
+               z = c(7, 8, 9))
+
+Df_7 <- tibble(y = c(6, 7),
+               z = c(9, 10),
+               x = c(3, 4))
+
+
+bind_rows(Df_1, Df_2)
+bind_rows(Df_2, Df_1)
+bind_cols(Df_1, Df_3)
+
+
+inner_join(Df_a, Df_b)
+left_join(Df_a, Df_b)
+right_join(Df_a, Df_b)
+full_join(Df_a, Df_b)
+
+blp_stimuli <- read_csv("https://raw.githubusercontent.com/mark-andrews/bas2021datascience/master/data/blp_stimuli.csv")
+
+
+inner_join(blp_df, blp_stimuli)
+left_join(blp_df, blp_stimuli)
+
+all_equal(inner_join(blp_df, blp_stimuli),
+          left_join(blp_df, blp_stimuli))
+
+
+right_join(blp_df, blp_stimuli)
+
+
+inner_join(Df_4, Df_5) # errors
+inner_join(Df_4, Df_5, by = c('x' = 'a'))
+
+
+# Reshaping with pivots ---------------------------------------------------
+
+recall_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/bas2021datascience/master/data/repeated_measured_a.csv")
+
+recall_df_long <- pivot_longer(recall_df, 
+                               -Subject, 
+                               names_to = 'condition',
+                               values_to = 'score')
+
+
+pivot_wider(recall_df_long,
+            names_from = 'condition',
+            values_from = 'score')
+
+
+recall_df_b <- read_csv("https://raw.githubusercontent.com/mark-andrews/bas2021datascience/master/data/repeated_measured_b.csv")
+
+
+pivot_longer(recall_df_b,
+             cols = -Subject,
+             names_to = 'condition',
+             values_to = 'score')
+
+
+# reading multiple csv files ----------------------------------------------
+# See the following blog post: https://www.mjandrews.org/blog/readmultifile/
+library(fs)
+
+data_df_list <- dir_ls('data/exp_data') %>% 
+  map(read_csv, id = 'name')
+
+bind_rows(data_df_list, .id = 'name')
